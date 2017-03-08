d6748ad4542ba830d5cc97c1e523746f4742054c
yvzkr <yavuzkuru3@gmail.com>
2017-03-08 14:17:07 +0300

    sequence.fasta dosyası eklendi ve DNA.py oluşturuldu

---
+++ DNA.py
@@ -0,0 +1,49 @@
+import re
+
+
+bazlar=""
+uzunluk=0
+with open("sequence.fasta", "r") as f:
+    f.readline()
+    veri=f.read()
+    for harf in veri:
+        nesne=re.search("\\n",harf)
+        if nesne:
+            pass
+        else:
+            uzunluk=uzunluk+1
+            bazlar=bazlar+harf
+
+
+#print(bazlar,"   ", uzunluk)
+uzunluk=35
+aralık=5
+baslangic=0
+start=0
+
+T=0
+G=0
+C=0
+for i in range(baslangic,uzunluk,aralık):
+    print(bazlar[i:aralık+i])
+    blok=bazlar[i:aralık+i]
+    print("blok da yazan",blok)
+    #sGC hesaplama
+    A=0
+    T=0
+    G=0
+    C=0
+    for a in blok:
+        if a=="A":
+            A=A+1
+        elif a=="T":
+            T=T+1
+        elif a=="G":
+            G=G+1
+        elif a=="C":
+            C=C+1
+    print("A : {}\n T : {}\n G : {}\n C : {}\n ".format(A,T,G,C))
+
+
+
+#print("A : {}\n T : {}\n G : {}\n C : {}\n ".format(A,T,G,C))
