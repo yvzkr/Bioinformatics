9666cfa5379aa0e51b9c38c982a8628461b2630c
yvzkr <yavuzkuru3@gmail.com>
2017-03-14 15:10:44 +0300

    düzeltme yapıldı

--- DNA.py
+++ DNA.py
@@ -1,10 +1,11 @@
 import re
 import matplotlib.pyplot as plt
+import collection
 
 
 sequence=""
 uzunluk=0
-with open("egfp.fasta", "r") as f:
+with open("pEGFP.fasta", "r") as f:
     header=f.readline()
     veri=f.read()
     for harf in veri:
@@ -15,37 +16,30 @@ with open("egfp.fasta", "r") as f:
             uzunluk=uzunluk+1
             sequence=sequence+harf
 
-print(uzunluk,"\n",header)
+#print(uzunluk,"\n",header)
 
-listex,listey=[],[]
-adet=0
 def GCKaymasi():
     #print(sequence,"   ", uzunluk)
     #uzunluk=100
-    aralık=50
+    aralık=10
     baslangic=0
-    global adet
+    adet=0
     topsGC,topsATc=0.0000,0.00000
-    global listex
-    global listey
+    listex,listey=[],[]
     for i in range(baslangic,uzunluk,aralık):
         #print(sequence[i:aralık+i])
         adet=adet+1
         blok=sequence[i:aralık+i]
-        print("blok da yazan",blok)
+        #print("blok da yazan",blok)
         #sGC hesaplama
 
         A,T,G,C=0,0,0,0
         for a in blok:
-            if a=="A":
-                A=A+1
-            elif a=="T":
-                T=T+1
-            elif a=="G":
+            if a=="G":
                 G=G+1
             elif a=="C":
                 C=C+1
-        print("A : {}\n T : {}\n G : {}\n C : {}\n ".format(A,T,G,C))
+        #print("A : {}\n T : {}\n G : {}\n C : {}\n ".format(A,T,G,C))
 
         try:
             sGC=(float)(G-C)/(G+C)
@@ -53,20 +47,62 @@ def GCKaymasi():
         except ZeroDivisionError:
             sGC=0
 
-        print("sonuçlar",sGC)
+        #print("sonuçlar",sGC)
 
         topsGC=topsGC+sGC
         listex=listex+[adet]
         listey=listey+[topsGC]
-def grafikciz():
+    ######
+    grafikciz(adet,listex,listey)
+
+
+def ATKaymasi():
+    #print(sequence,"   ", uzunluk)
+    #uzunluk=100
+    aralık=50
+    baslangic=0
+    adet=0
+    topsAT=0.00000
+    listex,listey=[],[]
+    for i in range(baslangic,uzunluk,aralık):
+        #print(sequence[i:aralık+i])
+        adet=adet+1
+        blok=sequence[i:aralık+i]
+        #print("blok da yazan",blok)
+        #sGC hesaplama
+        A,T,G,C=0,0,0,0
+        for a in blok:
+            if a=="A":
+                A=A+1
+            elif a=="T":
+                T=T+1
+        #print("A : {}\n T : {}\n G : {}\n C : {}\n ".format(A,T,G,C))
+
+        try:
+            #sGC=(float)(G-C)/(G+C)
+            sAT=(float)(A-T)/(A+T)
+        except ZeroDivisionError:
+            sAT=0
+
+        #print("sonuçlar",sAT)
+
+        topsAT=topsAT+sAT
+        listex=listex+[adet]
+        listey=listey+[topsAT]
+    ######
+    grafikciz(adet,listex,listey)
+
+
+
+def grafikciz(adet,listex,listey):
     #print("GC kayması: ",sGC," AT kayması",sAT,"\n topsGC :",topsGC)
     print(adet,"====",len(sequence))
     plt.plot(listex, listey)#
     #plt.axis([0, 10, 0, 20])
     plt.show()
 
+def CpG():
 
-GCKaymasi()
-grafikciz()
+
 
     #print("A : {}\n T : {}\n G : {}\n C : {}\n ".format(A,T,G,C))
