2b618ae1a1d6dc1865bb3edf505010b6896ae376
yvzkr <yavuzkuru3@gmail.com>
2017-03-16 11:25:33 +0300

    Collection kütüphanesinden Counter modülüne geçildi

--- DNA.py
+++ DNA.py
@@ -1,104 +1,48 @@
 import re
-import matplotlib.pyplot as plt
 from collections import Counter
+import matplotlib.pyplot as plt
 
-
-sequence=""
-uzunluk=0
-with open("sequence.fasta") as f:
-    fasta=f.read()
+with open("coli.fasta") as f:
+    fasta = f.read()
 for genome in fasta.split(">")[1:]:
-    header, genomeseq=genome.splitlines()[0],"".join(genome.splitlines()[1:])
-    counts=Counter(genomeseq)
+    header, genomeseq = genome.splitlines()[0], "".join(genome.splitlines()[1:])
+    counts = Counter(genomeseq)
+    genomeSeqlen=0
+    genomeSeqlen=counts["A"]+counts["T"]+counts["G"]+counts["C"]
 
 
-#print(uzunluk,"\n",header)
+    #print("Counts for the genome:", header)
+    #print("A:", counts["A"])
+    #print("T:", counts["T"])
+    #print("G:", counts["G"])
+    #print("C:", counts["C"])
+#print(genomeseq)
 
-def GCKaymasi():
-    #print(sequence,"   ", uzunluk)
-    #uzunluk=100
-    aralık=10
-    baslangic=0
-    adet=0
-    topsGC,topsATc=0.0000,0.00000
-    listex,listey=[],[]
-    for i in range(baslangic,uzunluk,aralık):
-        #print(sequence[i:aralık+i])
-        adet=adet+1
-        blok=sequence[i:aralık+i]
-        #print("blok da yazan",blok)
-        #sGC hesaplama
-
-        A,T,G,C=0,0,0,0
-        for a in blok:
-            if a=="G":
-                G=G+1
-            elif a=="C":
-                C=C+1
-        #print("A : {}\n T : {}\n G : {}\n C : {}\n ".format(A,T,G,C))
-
-        try:
-            sGC=(float)(G-C)/(G+C)
-            #sAT=(float)(A-T)/(A+T)
-        except ZeroDivisionError:
-            sGC=0
-
-        #print("sonuçlar",sGC)
-
-        topsGC=topsGC+sGC
-        listex=listex+[adet]
-        listey=listey+[topsGC]
-    ######
-    grafikciz(adet,listex,listey)
-
-
-def ATKaymasi():
-    #print(sequence,"   ", uzunluk)
-    #uzunluk=100
-    aralık=50
-    baslangic=0
-    adet=0
-    topsAT=0.00000
-    listex,listey=[],[]
-    for i in range(baslangic,uzunluk,aralık):
-        #print(sequence[i:aralık+i])
-        adet=adet+1
-        blok=sequence[i:aralık+i]
-        #print("blok da yazan",blok)
-        #sGC hesaplama
-        A,T,G,C=0,0,0,0
-        for a in blok:
-            if a=="A":
-                A=A+1
-            elif a=="T":
-                T=T+1
-        #print("A : {}\n T : {}\n G : {}\n C : {}\n ".format(A,T,G,C))
-
-        try:
-            #sGC=(float)(G-C)/(G+C)
-            sAT=(float)(A-T)/(A+T)
-        except ZeroDivisionError:
-            sAT=0
-
-        #print("sonuçlar",sAT)
-
-        topsAT=topsAT+sAT
-        listex=listex+[adet]
-        listey=listey+[topsAT]
-    ######
-    grafikciz(adet,listex,listey)
-
-
-
-def grafikciz(adet,listex,listey):
-    #print("GC kayması: ",sGC," AT kayması",sAT,"\n topsGC :",topsGC)
-    print(adet,"====",len(sequence))
-    plt.plot(listex, listey)#
-    #plt.axis([0, 10, 0, 20])
+def Draw_graphic(liste):
+    plt.plot(range(0,len(liste)),liste)
     plt.show()
 
-def CpG():
+def c_gc_skewness(pieceSeqLen):
+    #counts = Counter(genomeseq)
+    listey=[0]
+    for i in range(0,genomeSeqlen,pieceSeqLen):
+        genome=genomeseq[i:i+pieceSeqLen]
+        countsGenome=Counter(genome)
+        G,C=countsGenome["G"],countsGenome["C"]
+        try:
+            sGC=(G-C)/(G+C)
+        except ZeroDivisionError:
+            sGC=0
+            #print("sGC:",sGC,"son eleman",listey[-1])
+            #print(genome," G nin sayisi= ",G," C nin sayisi= ",C,"gc kaymasi: ",sGC)
+        listey.append((listey[-1]+sGC))
+
+        #print(genome," G nin sayisi= ",G," C nin sayisi= ",C,"gc kaymasi: ",sGC)
+    Draw_graphic(listey)
 
 
 
-    #print("A : {}\n T : {}\n G : {}\n C : {}\n ".format(A,T,G,C))
+
+
+
+c_gc_skewness(50)
